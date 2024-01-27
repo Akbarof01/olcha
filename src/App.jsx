07bd0b7621f312a3a424b864e2294e6687da46fe
Akbarof01 <akhbbarov01@gmail.com>
2024-01-28 04:58:35 +0500

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import "./App.css";
+import MySviper from "./components/navbar/MySviper";
 import Navbar from "./components/navbar/Navbar";
+
 
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <MySviper />
     </div>
   );
 }
